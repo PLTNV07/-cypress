8ee6456fe43fbaa2ca67c8332f58e35fdd2a5636
PLTNV07 <147551901+PLTNV07@users.noreply.github.com>
2024-01-22 20:15:50 +0300

    Update pokemon.cy.js

--- new_cypress-main/cypress/e2e/pokemon.cy.js
+++ new_cypress-main/cypress/e2e/pokemon.cy.js
@@ -1,12 +1,11 @@
 describe('Покупка аватара', function () {
-   
-   it('Покупка нвового аватара', function () {
+   it('e2e тест на покупку нового аватара для тренера', function () {
         cy.visit('https://pokemonbattle.me/');
         cy.get(':nth-child(1) > .auth__input').type('PlatonovaTj@yandex.ru');
         cy.get('#password').type('DandyLucky2006');
         cy.get('.auth__button').click();
         cy.get('.header__btns > [href="/shop"]').click();
-        cy.get(':nth-child(10) > .shop__button').click();
+        cy.get('.shop__list > li').not('.feature-empty').children('.shop__button').eq(0).click();
         cy.get('.pay__payform-v2 > :nth-child(2) > .pay_base-input-v2').type('4620869113632996');
         cy.get(':nth-child(1) > .pay_base-input-v2').type('1225');
         cy.get('.pay-inputs-box > :nth-child(2) > .pay_base-input-v2').type('125');
