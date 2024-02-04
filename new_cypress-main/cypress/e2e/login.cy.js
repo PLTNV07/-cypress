2002711043d0c2e9ae5a4078b3081b57833b8610
Platonova Tatyana <147551901+PLTNV07@users.noreply.github.com>
2024-02-04 18:06:13 +0300

    login.cy.js

--- new_cypress-main/cypress/e2e/login.cy.js
+++ new_cypress-main/cypress/e2e/login.cy.js
@@ -1,8 +1,8 @@
 describe('Автотесты на форму логина и пароля', function () {
    it('Позитивный автотест на форму авторизации', function () {
         cy.visit('https://login.qa.studio/');
-        cy.get('#mail').type('german@dolnikov.ru');
-        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#mail').type('***');
+        cy.get('#pass').type('***');
         cy.get('#loginButton').click();
         cy.get('#messageHeader').should('be.visible').contains('Авторизация прошла успешно');
         cy.get('#exitMessageButton > .exitIcon').should('be.visible');
@@ -12,15 +12,15 @@ describe('Автотесты на форму логина и пароля', func
        cy.visit('https://login.qa.studio/');
        cy.get('#forgotEmailButton').click();
        cy.get('#forgotForm > .header').should('be.visible').contains('Восстановите пароль');
-       cy.get('#mailForgot').type('german@dolnikov.ru');
+       cy.get('#mailForgot').type('***');
        cy.get('#restoreEmailButton').click();
        cy.get('#messageHeader').should('be.visible').contains('Успешно отправили пароль на e-mail');
        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
 })
    it('Негавтивный автотест на проверку неверного логина', function () {
         cy.visit('https://login.qa.studio/');
-        cy.get('#mail').type('german@dolnikov.com');
-        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#mail').type('***');
+        cy.get('#pass').type('***');
         cy.get('#loginButton').click();
         cy.get('#messageHeader').should('be.visible').contains('Такого логина или пароля нет');
         cy.get('#exitMessageButton > .exitIcon').should('be.visible');
@@ -28,8 +28,8 @@ describe('Автотесты на форму логина и пароля', func
 })
    it('Негавтивный автотест на проверку неверного пароля', function () {
         cy.visit('https://login.qa.studio/');
-        cy.get('#mail').type('german@dolnikov.ru');
-        cy.get('#pass').type('iLoveqastudio!');
+        cy.get('#mail').type('***');
+        cy.get('#pass').type('***');
         cy.get('#loginButton').click();
         cy.get('#messageHeader').should('be.visible').contains('Такого логина или пароля нет');
         cy.get('#exitMessageButton > .exitIcon').should('be.visible');
@@ -37,8 +37,8 @@ describe('Автотесты на форму логина и пароля', func
 })
    it('Негативный автотест на проверку валидации', function () {
         cy.visit('https://login.qa.studio/');
-        cy.get('#mail').type('germandolnikov.ru');
-        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#mail').type('***');
+        cy.get('#pass').type('***');
         cy.get('#loginButton').click();
         cy.get('#messageHeader').should('be.visible').contains('Нужно исправить проблему валидации');
         cy.get('#exitMessageButton > .exitIcon').should('be.visible');
@@ -46,8 +46,8 @@ describe('Автотесты на форму логина и пароля', func
 
    it('Автотест на проверку приведения к строчным буквам логина', function () {
         cy.visit('https://login.qa.studio/');
-        cy.get('#mail').type('GerMan@Dolnikov.ru');
-        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#mail').type('***');
+        cy.get('#pass').type('***');
         cy.get('#loginButton').click();
         cy.get('#messageHeader').should('be.visible').contains('Авторизация прошла успешно');
         cy.get('#exitMessageButton > .exitIcon').should('be.visible');
